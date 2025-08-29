28acfffc22e8f4ff57ad93b7e7135d59f9232003
Andalusi <49962792+andalusi7@users.noreply.github.com>
2025-08-29 13:46:19 +0300

    Update database.ts

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -2,19 +2,23 @@ export default ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      connectionString: env("DATABASE_URL"),
       host: env("DATABASE_HOST", "localhost"),
       port: env.int("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME", "strapi"),
       user: env("DATABASE_USERNAME", "strapi"),
       password: env("DATABASE_PASSWORD", "strapi"),
-     
       schema: env("DATABASE_SCHEMA", "public"),
+      ssl: env.bool("DATABASE_SSL", false)
+        ? { rejectUnauthorized: false }
+        : false,
     },
     pool: {
       min: env.int("DATABASE_POOL_MIN", 2),
       max: env.int("DATABASE_POOL_MAX", 10),
     },
-    acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
+    acquireConnectionTimeout: env.int(
+      "DATABASE_CONNECTION_TIMEOUT",
+      60000
+    ),
   },
 });
