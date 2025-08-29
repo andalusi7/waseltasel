b72d9e180b4d346eec4d4da21b3571ecbfbceec4
Andalusi <49962792+andalusi7@users.noreply.github.com>
2025-08-29 13:56:04 +0300

    Update server.ts

--- backend/config/server.ts
+++ backend/config/server.ts
@@ -1,6 +1,6 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 8080), // Azure سيضبط PORT تلقائياً
   app: {
     keys: env.array('APP_KEYS', ['defaultKey1', 'defaultKey2']),
   },
